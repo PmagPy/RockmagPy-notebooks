94bf130e6c91fa5d972add10151f44fb1ce9f416
peat22 <peat@umn.edu>
2024-02-21 11:31:54 -0600

    first commit
    a notebook that mostly does not work

--- .ipynb_checkpoints/rockmag-checkpoint.py
+++ .ipynb_checkpoints/rockmag-checkpoint.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import ipywidgets as widgets
@@ -43,35 +42,6 @@ def extract_mpms_data(df, specimen_name):
 
     return fc_data, zfc_data, rtsirm_cool_data, rtsirm_warm_data
 
-def extract_hysteresis_data(df, specimen_name):
-    """
-    Extracts and separates Hysteresis data 
-    for a specific specimen from a dataframe.
-
-    This function filters data for a given specimen and separates it based on 
-    different MagIC measurement method codes. It specifically looks for data 
-    corresponding to 'LP-HYS' Hysteresis Data
-
-    Parameters:
-        df (pandas.DataFrame): The dataframe containing MPMS measurement data.
-        specimen_name (str): The name of the specimen to filter data for.
-
-    Returns:
-        tuple: A tuple containing four pandas.DataFrames:
-            - fc_data: Data filtered for 'LP-FC' method if available, otherwise an empty DataFrame.
-            - zfc_data: Data filtered for 'LP-ZFC' method if available, otherwise an empty DataFrame.
-            - rtsirm_cool_data: Data filtered for 'LP-CW-SIRM:LP-MC' method if available, otherwise an empty DataFrame.
-            - rtsirm_warm_data: Data filtered for 'LP-CW-SIRM:LP-MW' method if available, otherwise an empty DataFrame.
-
-    Example:
-        >>> fc, zfc, rtsirm_cool, rtsirm_warm = extract_mpms_data(measurements_df, 'Specimen_1')
-    """
-
-    specimen_df = df[df['specimen'] == specimen_name]
-
-    hyst_data = specimen_df[specimen_df['method_codes'].str.contains('LP-HYS', na=False)]
-
-    return hyst_data
 
 def get_plotly_marker(matplotlib_marker):
     """
@@ -222,64 +192,6 @@ def plot_mpms_data(fc_data, zfc_data, rtsirm_cool_data, rtsirm_warm_data,
         if return_figure:
             return fig
 
-def plot_hyst_data(hyst_data,
-                   hyst_color='#1f77b4',
-                   hyst_marker='.',
-                   symbol_size=5, use_plotly=False, return_figure=False):
-    """
-    Plots hysteresis data using either Matplotlib or Plotly.
-
-    Parameters:
-        hyst_data (DataFrame): DataFrames containing the hysteresis data.
-        hyst_color: HEX color codes for each plot.
-        hyst_marker (str): Marker symbols for each plot.
-        symbol_size (int): Size of the markers in matplotlib, symbol size in plotly is fixed.
-        use_plotly (bool): If True, uses Plotly for plotting. Otherwise, uses Matplotlib.
-        
-    Returns:
-        fig: The matplotlib.figure.Figure object containing the plot (only when using Matplotlib).
-    """
-
-    if use_plotly:
-        rows, cols = (1, 1)
-        fig = make_subplots(rows=rows, cols=cols)
-        
-        # Add original data traces
-        fig.add_trace(go.Scatter(x=hyst_data['meas_field_dc'], y=hyst_data['magn_mass'], mode='markers+lines', name='Hyst', marker=dict(color=hyst_color)), row=1,    col=1)
-        
-        # Update layout and axis titles
-        # Set y-axis label for the first row to 'M (Am2/kg)'
-        fig.update_yaxes(title_text="M (Am2/kg)", row=1, col=1)
-        fig.update_yaxes(title_text="M (Am2/kg)", row=1, col=2)
-
-        fig.update_xaxes(title_text="Field (Tesla)", row=1, col=1)
-
-
-        fig.update_layout(title="Hysteresis Data")
-        
-        fig.show()
-            
-    else:
-        # Matplotlib plotting
-        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
-            
-        # Plot original data
-
-        axs[0].plot(hyst_data['meas_field_dc'], hyst_data['magn_mass'], color=hyst_color, marker=hyst_marker, linestyle='-', markersize=symbol_size, label='Loop')
-        axs[1].plot(hyst_data['meas_field_dc'], hyst_data['magn_mass'], color=hyst_color, marker=hyst_marker, linestyle='-', markersize=symbol_size, label='Loop')
-        for ax in axs:
-            ax.set_xlabel("Field (Tesla)")
-            ax.set_ylabel("Magnetization (Am$^2$/kg)")
-            ax.legend()
-            ax.grid(True)
-            #ax.set_xlim(0, 300)
-    
-
-        fig.tight_layout()
-        plt.show()
-        
-        if return_figure:
-            return fig
 
 def make_mpms_plots(measurements):
     """
@@ -339,64 +251,7 @@ def make_mpms_plots(measurements):
     # Display UI components
     display(specimen_dropdown, plot_choice, out)
            
-def make_hyst_plots(measurements):
-    """
-    Create a UI for specimen selection and dynamically update Hysteresis loop plots based on the selected
-    specimen and plot library choice. This version adds event handlers to ensure updates occur
-    upon initial selection.
-
-    Parameters:
-    experiments : pandas.DataFrame
-        The dataframe containing experiment data with columns including 'specimen' and 'method_codes'.
-    measurements : pandas.DataFrame
-        The dataframe containing measurement data used for plotting Hysteresis loop data.
-    """
-    # Filter to get specimens with desired method codes
-    experiments = measurements.groupby(['specimen', 'method_codes']).size().reset_index().iloc[:, :2]
-    filtered_experiments = experiments[experiments['method_codes'].isin(['LP-HYS',])]
-    specimen_options = filtered_experiments['specimen'].unique().tolist()
-
-    # Dropdown for specimen selection
-    specimen_dropdown = widgets.Dropdown(
-        options=specimen_options,
-        description='Specimen:',
-        value=specimen_options[0]
-    )
-
-    # Radio buttons for plot library choice
-    plot_choice = widgets.RadioButtons(
-        options=[('matplotlib', False), ('plotly', True)],
-        description='Plot with:',
-        disabled=False
-    )
-
-    # Interactive output container
-    out = widgets.Output()
-
-    def update_hyst_plots(specimen_name, use_plotly):
-        """
-        Update hysteresis loop based on the selected specimen and plotting library choice.
-        """
-        with out:
-            out.clear_output(wait=True)
-            hyst_data = extract_hysteresis_data(measurements, specimen_name)
-            plot_hyst_data(hyst_data, use_plotly=use_plotly)
-
-    def on_specimen_change(change):
-        update_hyst_plots(change['new'], plot_choice.value)
-
-    def on_plot_choice_change(change):
-        update_hyst_plots(specimen_dropdown.value, change['new'])
-
-    specimen_dropdown.observe(on_specimen_change, names='value')
-    plot_choice.observe(on_plot_choice_change, names='value')
-
-    # Initial plot to ensure something is displayed right away
-    update_hyst_plots(specimen_dropdown.value, plot_choice.value)
-
-    # Display UI components
-    display(specimen_dropdown, plot_choice, out)
-
+        
 def thermomag_derivative(temps, mags):
     """
     Calculates the derivative of magnetization with respect to temperature.
@@ -509,8 +364,7 @@ def verwey_estimate(temps, mags,
                     excluded_t_min = 75,
                     excluded_t_max = 150,
                     poly_deg = 3,
-                    plot_zero_crossing = False,
-                    plot_title = None):
+                    plot_zero_crossing = False):
     
     temps.reset_index(drop=True, inplace=True)
     mags.reset_index(drop=True, inplace=True)
@@ -557,8 +411,9 @@ def verwey_estimate(temps, mags,
     verwey_estimate = zero_crossing(temps_dM_dT_background, mgt_dM_dT, 
                                     make_plot=plot_zero_crossing, xlim=(excluded_t_min, excluded_t_max))
     
-    remanence_loss = mgt_curve.iloc[0] - mgt_curve.iloc[-1]
-    
+    # print('The r-squared value for the background fit is: ' + str(round(r_squared,3)))
+    # print('The Verwey temperature estimate is: ' + str(round(verwey_estimate,1)) + ' K')
+
     fig = plt.figure(figsize=(12,5))
     ax0 = fig.add_subplot(1,2,1)
     ax0.plot(temps, mags, '.-', color='red', label='measurement')
@@ -573,8 +428,6 @@ def verwey_estimate(temps, mags,
     ax0.legend(loc='upper right')
     ax0.grid(True)
     ax0.ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
-    if plot_title is not None:
-        ax0.set_title(plot_title)
 
     ax1 = fig.add_subplot(1,2,2)
     ax1.plot(dM_dT_df['T'], dM_dT_df['dM_dT'], '.-', color='red', label='measurement')
@@ -584,24 +437,14 @@ def verwey_estimate(temps, mags,
     ax1.plot(verwey_estimate, verwey_y_value, '*', color='pink', markersize=10,
          markeredgecolor='black', markeredgewidth=1,
          label='Verwey estimate' + ' (' + str(round(verwey_estimate,1)) + ' K)')
-    rectangle = patches.Rectangle((excluded_t_min, ax1.get_ylim()[0]), excluded_t_max - excluded_t_min, 
-                                  ax1.get_ylim()[1] - ax1.get_ylim()[0], 
-                                  linewidth=0, edgecolor=None, facecolor='gray', 
-                                  alpha=0.3)
-    ax1.add_patch(rectangle)
-    rect_legend_patch = patches.Patch(color='gray', alpha=0.3, label='excluded from background fit')
-    handles, labels = ax1.get_legend_handles_labels()
-    handles.append(rect_legend_patch)  # Add the rectangle legend patch
-    ax1.legend(handles=handles, loc='lower right')
     ax1.set_ylabel('dM/dT (Am$^2$/kg/K)')
     ax1.set_xlabel('T (K)')
+    ax1.legend(loc='lower right')
     ax1.grid(True)
     ax1.ticklabel_format(axis='y', style='scientific', scilimits=(0,0))
-    if plot_title is not None:
-        ax1.set_title(plot_title)
     #plt.show()
 
-    return verwey_estimate, remanence_loss
+    return verwey_estimate
 
 
 def interactive_verwey_estimate(measurements, specimen_dropdown, method_dropdown):
@@ -617,12 +460,9 @@ def interactive_verwey_estimate(measurements, specimen_dropdown, method_dropdown
         temps = zfc_data['meas_temp']
         mags = zfc_data['magn_mass']
 
-    # Determine a fixed width for the descriptions to align the sliders
-    description_width = '250px'  # Adjust this based on the longest description
-    slider_total_width = '600px'  # Total width of the slider widget including the description
-
-    description_style = {'description_width': description_width}
-    slider_layout = widgets.Layout(width=slider_total_width)  # Set the total width of the slider widget
+    # Adjust the layout for the sliders
+    slider_layout = widgets.Layout(width='500px')
+    description_style = {'description_width': 'initial'}
 
     # Update sliders to use IntRangeSlider
     background_temp_range_slider = widgets.IntRangeSlider(
@@ -753,183 +593,6 @@ def interactive_verwey_specimen_method_selection(measurements):
     display(ui_layout)
     
     return specimen_dropdown, method_dropdown
-
-def y_polate(n, xarr, yarr, xval, yval):
-    # linear interpolation/extrapolation, returns best-fit y for a specified x (e.g. for Mr)
-    intercept, slope = linefit1(n, xarr, yarr)
-    ycal = intercept + slope * xval
-
-    return ycal
-
-
-def linefit1(n, xarr, yarr):
-    sum_x = sum(xarr)
-    sum_y = sum(yarr)
-    sum_xy = sum(x * y for x, y in zip(xarr, yarr))
-    sum_x_squared = sum(x ** 2 for x in xarr)
-
-    slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x ** 2)
-    intercept = (sum_y - slope * sum_x) / n
-
-    return intercept, slope
-
-def branch_sub(n_loop, grid_moments):
-
-    j = n_loop // 2
-    moment_sub = np.ndarray([j])
-    for i in range (j):
-        moment_sub[i] = grid_moments[i]-grid_moments[n_loop-1-i]
-    return moment_sub
-
-def loop_grid(n_loop, polydegree, nsmooth, loop_fields, loop_moments, B_offset, M_offset):
-
-    n2 = nsmooth // 2  # make nsmooth odd; n2=# on each side, + 1 in center
-    xvals = np.ndarray([nsmooth])
-    yvals = np.ndarray([nsmooth])
-    xvals[:]=0
-    yvals[:]=0
-
-    grid_fields = np.ndarray([n_loop])
-    grid_moments = np.ndarray([n_loop])
-    grid_fields[:] = 0
-    grid_moments[:] = 0
-
-
-    field_step = loop_fields[0] / n_loop * 4
-    grid_fields[0] = round(1000 * (loop_fields[0] - abs(B_offset))) / 1000  # nearest mT
-    grid_fields[n_loop // 2] = -round(1000 * (loop_fields[0] - abs(B_offset))) / 1000  # nearest mT
-    field_step = grid_fields[0] / (n_loop - 2) * 4
-    for i in range(1, n_loop // 2):  #was +1, but this is 0 bassed array
-        grid_fields[i] = grid_fields[i - 1] - field_step
-        grid_fields[i + n_loop // 2] = grid_fields[i - 1 + n_loop // 2] + field_step
-
-
-    j = 1
-    grid_moments[0] = loop_moments[0] - M_offset
-
-    for i in range(1, n_loop // 2):
-        while not (loop_fields[j] - B_offset >= grid_fields[i]): # or (j <= 2):
-            if j<=1:
-                break
-            if j > 1:
-                j -= 1
-        while not (loop_fields[j] - B_offset <= grid_fields[i]) or (j >= n_loop // 2):
-            if j < n_loop // 2:
-                j += 1
-
-        k = 1
-        xvals[1] = loop_fields[j] - B_offset
-        xvals[2] = loop_fields[j - k] - B_offset
-        yvals[1] = loop_moments[j] - M_offset
-        yvals[2] = loop_moments[j - k] - M_offset
-
-        while abs(xvals[1] - xvals[2]) <= field_step / 2:
-            k += 1
-            xvals[2] = loop_fields[j - k] - B_offset
-            yvals[2] = loop_moments[j - k] - M_offset
-
-        ycal = y_polate(2, xvals, yvals, grid_fields[i], grid_moments[i])
-        grid_moments[i]=ycal
-        #print(grid_moments[i])
-    j -= 1
-    grid_moments[n_loop // 2] = loop_moments[n_loop // 2] - M_offset
-
-    for i in range(n_loop // 2, n_loop):
-        while not (loop_fields[j] - B_offset <= grid_fields[i]): # or (j <= n_loop // 2 + 1):
-            if j <= n_loop // 2 + 1:
-                break
-            if j > n_loop // 2 + 1:
-                j -= 1
-        while not (loop_fields[j] - B_offset >= grid_fields[i]) or (j >= n_loop):
-            if j < n_loop:
-                j += 1
-
-        k = 1
-        xvals[1] = loop_fields[j] - B_offset
-        xvals[2] = loop_fields[j - k] - B_offset
-        yvals[1] = loop_moments[j] - M_offset
-        yvals[2] = loop_moments[j - k] - M_offset
-
-        while abs(xvals[1] - xvals[2]) <= field_step / 2:
-            k += 1
-            xvals[2] = loop_fields[j - k] - B_offset
-            yvals[2] = loop_moments[j - k] - M_offset
-
-        ycal = y_polate(2, xvals, yvals, grid_fields[i], grid_moments[i])
-        grid_moments[i] = ycal
-        #print(grid_moments[i])
-    j = 2
-
-    if polydegree > 1:
-        for i in range(n2 + 1, n_loop // 2 - (n2 + 1) + 1):
-            while not (loop_fields[j] - B_offset >= grid_fields[i]): # or (j <= 2):
-                if j<=2:
-                    break
-                if j > 2:
-                    j -= 1
-            while not (loop_fields[j] - B_offset <= grid_fields[i]): # or (j >= n_loop // 2):
-                if j >= n_loop // 2:
-                    break
-                if j < n_loop // 2:
-                    j += 1
-
-            for k in range(-n2, n2 + 1):
-                x[k + n2 + 1] = loop_fields[j + k] - B_offset
-                y[k + n2 + 1] = loop_moments[j + k] - M_offset
-
-            # Call to polynomialfit1 function goes here
-            print(x)
-            r2 = float()
-            polynomialfit1(nsmooth, 0, polydegree, x, y) #polynomialfit1(nsmooth, 0, polydegree, x, y, y, r2)
-
-
-            y1 = 0
-            x1 = 1
-
-            for k in range(polydegree + 1):
-                if k > 0:
-                    x1 *= grid_fields[i]
-                y1 += y[k + 1] * x1
-
-            grid_moments[i] = y1
-
-        j -= 1
-
-        for i in range(n_loop // 2 + n2 + 1, n_loop - (n2 + 1) + 1):
-            while not (loop_fields[j] - B_offset <= grid_fields[i]) or (j <= n_loop // 2 + 1):
-                if j > n_loop // 2 + 1:
-                    j -= 1
-            while not (loop_fields[j] - B_offset >= grid_fields[i]) or (j >= n_loop):
-                if j < n_loop:
-                    j += 1
-
-            try:
-                for k in range(-n2, n2 + 1):
-                    x[k + n2 + 1] = loop_fields[j + k] - B_offset
-                    y[k + n2 + 1] = loop_moments[j + k] - M_offset
-            except:
-                x1 = 0
-
-            if j + k <= n_loop:
-                # Call to polynomialfit1 function goes here
-                # polynomialfit1(nsmooth, 0, polydegree, x, y, y, r2)
-                # The implementation of polynomialfit1 function is needed
-
-                y1 = 0
-                x1 = 1
-
-                for k in range(polydegree + 1):
-                    if k > 0:
-                        x1 *= grid_fields[i]
-                    y1 += y[k + 1] * x1
-
-                grid_moments[i] = y1
-
-    return grid_fields, grid_moments
-
-
-
-
 
 
 # Function that needs to be further developed that could be used to read in the data from the compact MagIC format
